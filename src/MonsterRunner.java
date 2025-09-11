c03aee008dd27f8b37d803a2188b1c6d55a79cfc
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-11 09:59:51 -0600

    a

--- src/MonsterRunner.java
+++ src/MonsterRunner.java
@@ -1,8 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Math;
 public class MonsterRunner {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		File file = new File("emails.txt");
+		Scanner scan = new Scanner(file);
+		while(scan.hasNextLine()) {
+			String line = scan.nextLine();
+			Scanner lineScanner = new Scanner(line);
+			String email = lineScanner.next();
+			
+			System.out.print(email.substring(0, email.indexOf("@")));
+			System.out.print(", " + pass + "\n");
+		}
+		scan.close();
 
 	}
-
 }
