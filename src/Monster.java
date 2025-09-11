6226c18dda0d83a1f97307367ac396dc112be0ae
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-11 09:53:58 -0600

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -16,5 +16,34 @@ public class Monster {
 	public void setType(String t) {
 		type = t;
 	}
+	public void setLevel(int l) {
+		level = l;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getType() {
+		return type;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public static Monster basicBattle(Monster a, Monster b) {
+		if(a.getLevel() < b.getLevel()) {return b;}
+		return a;
+	}
+	public static Monster typeBattle(Monster a, Monster b) {
+		if(a.getType() == "Water" && b.getType() == "Fire") {return b;}
+		if(a.getType() == "Earth" && b.getType() == "Water") {return b;}
+		if(a.getType() == "Fire" && b.getType() == "Earth") {return b;}
+		return a;
+	}
+	@Override
+	public String toString() {
+		return "Name: " + name + "\nType: " + type + "\nLevel: " + level;
+	}
+	public boolean equals(Monster m) {
+		return (name.equals(m.getName())) && (type.equals(m.getType()));
+	}
 	
 }
