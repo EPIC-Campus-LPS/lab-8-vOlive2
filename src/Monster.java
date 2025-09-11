a8b0eee03a6e70d5daee235301d70117392e3cf2
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-11 09:41:13 -0600

    a

---
+++ src/Monster.java
@@ -0,0 +1,17 @@
+public class Monster {
+	private String name;
+	private String type;
+	private int level;
+	Monster(String n, String t) {
+		this(n, t, 1);
+	}
+	Monster(String n, String t, int l) {
+		setName(n);
+		setType(t);
+		setLevel(l);
+	}
+	public void setName(String n) {
+		name = n;
+	}
+	
+}
